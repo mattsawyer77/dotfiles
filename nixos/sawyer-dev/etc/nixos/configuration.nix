c4f554d1d1bfd7f61e01c442a614520dbbd8c353
Matt Sawyer <m.sawyer@f5.com>
2020-05-03 19:06:17 -0700

    update nix files for sawyer-dev

--- nixos/sawyer-dev/etc/nixos/configuration.nix
+++ nixos/sawyer-dev/etc/nixos/configuration.nix
@@ -8,64 +8,39 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./packages.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "sawyer-dev"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
   networking.useDHCP = false;
-  networking.interfaces.ens33.useDHCP = true;
+  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.wlp1s0.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    neovim
-    git
-    tmux
-    zsh
-    eternal-terminal
-    emacs
-    terraform
-    tflint
-    nodejs
-    rustup
-    cargo
-    cargo-tree
-    go
-    ripgrep
-    fd
-    awscli
-    aws-iam-authenticator
-    fzf-zsh
-    nix-zsh-completions
-    spaceship-prompt
-    zsh-autosuggestions
-    zsh-fast-syntax-highlighting
-    zsh-history-substring-search
-  ];
+  environment.variables = rec {
+    AWS_SDK_LOAD_CONFIG="1";
+    LANG="en_US.UTF-8";
+    LANGUAGE="en_US.UTF-8";
+    LC_ALL="en_US.UTF-8";
+    LESS="-F -g -i -M -R -X";
+    LESSCHARSET="utf-8";
+    TERM="xterm-24bit";
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -80,32 +55,50 @@
   services.eternal-terminal.enable = true;
 
   # Open ports in the firewall.
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
   networking.firewall.allowedTCPPorts = [ 22 2022 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  networking.firewall.allowPing = true;
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    promptInit = ''
+      eval "$(starship init zsh)"
+    '';
+    shellAliases = {
+      ssh = "TERM=xterm-256color ssh";
+      l ="exa -alF";
+    };
+    histSize = 500000;
+    setOptions = [
+      "SHARE_HISTORY"
+      "HIST_IGNORE_ALL_DUPS"
+      "HIST_IGNORE_DUPS"
+      "INC_APPEND_HISTORY"
+    ];
+    interactiveShellInit = ''
+      bindkey -v
+      # vi style incremental search
+      bindkey '^R' history-incremental-search-backward
+      bindkey '^S' history-incremental-search-forward
+      bindkey '^P' history-search-backward
+      bindkey '^N' history-search-forward
+      zstyle ':completion:*:*:*:*:*' menu select
+      source "$(fzf-share)/key-bindings.zsh"
+      source "$(fzf-share)/completion.zsh"
+    '';
+  };
 
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  programs.zsh.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sawyer = {
     isNormalUser = true;
@@ -122,5 +115,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  # system.stateVersion = "19.09"; # Did you read the comment?
+  # system.stateVersion = "20.03"; # Did you read the comment?
 }
