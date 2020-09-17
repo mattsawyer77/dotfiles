b9c1d1479193e2fb2a453d0639a1278503455b1c
Matt Sawyer <m.sawyer@f5.com>
2020-09-16 20:12:02 -0700

    restore/maximize doom on startup

--- doom/+editor.el
+++ doom/+editor.el
@@ -185,3 +185,22 @@
   ;; :hook (doom-first-input . zoom-mode)
   :config (setq zoom-size '(0.7 . 0.7)
                 zoom-ignored-major-modes '(treemacs-mode)))
+
+(when-let (dims (doom-store-get 'last-frame-size))
+  (cl-destructuring-bind ((left . top) width height fullscreen) dims
+    (setq initial-frame-alist
+          (append initial-frame-alist
+                  `((left . ,left)
+                    (top . ,top)
+                    (width . ,width)
+                    (height . ,height)
+                    (fullscreen . ,fullscreen))))))
+
+(defun save-frame-dimensions ()
+  (doom-store-put 'last-frame-size
+                  (list (frame-position)
+                        (frame-width)
+                        (frame-height)
+                        (frame-parameter nil 'fullscreen))))
+
+(add-hook 'kill-emacs-hook #'save-frame-dimensions)
