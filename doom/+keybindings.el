a26289b62b605ae42fe61576e3c6604522258e48
Matt Sawyer <m.sawyer@f5.com>
2022-09-22 12:25:17 -0700

    finally (hopefully) fixed doom key bindings

--- doom/+keybindings.el
+++ doom/+keybindings.el
@@ -3,101 +3,92 @@
 (map! "<mouse-4>" 'up-slightly)
 (map! "<mouse-5>" 'down-slightly)
 
-(map! :map general-override-mode-map
-      :after general-override-mode
-      :g "C-s" #'basic-save-buffer
-      :n "C-," (lambda ()
-                 (interactive)
-                     (dired-other-window "~/dotfiles/doom"))
-      :nv "#" #'evilnc-comment-or-uncomment-lines
-      :nv "SPC f c" #'ediff-buffers
-      :nv "<f9>" #'sawyer--light-switch
-      :nv "SPC \"" #'vertico-repeat-select
-      ;; centaur tabs
-      :niv "S-<left>" #'centaur-tabs-backward
-      :niv "S-<right>" #'centaur-tabs-forward
-      :niv "C-SPC" #'centaur-tabs-switch-group
-      :localleader
-      (:prefix ("d" . "debug")
-       :desc "DAP Debug" "d" #'dap-debug
-       :desc "DAP Hydra" "h" #'dap-hydra
-       :desc "DAP Disconnect" "Q" #'dap-disconnect
-       (:prefix ("b" . "breakpoint")
-        :desc "DAP Breakpoint Add" "a" #'dap-breakpoint-add
-        :desc "DAP Breakpoint Delete" "d" #'dap-breakpoint-delete
-        :desc "DAP Breakpoint Delete All" "D" #'dap-breakpoint-delete
-        )))
+(map!
+ :map general-override-mode-map
+ :g "C-s" #'basic-save-buffer
+ :n "C-," (lambda ()
+            (interactive)
+            (dired-other-window "~/dotfiles/doom"))
+ :nv "<f9>" #'sawyer--light-switch
+ (:leader
+  (:prefix ("f" . "file")
+   :desc "Ediff buffers" "c" #'ediff-buffers)
+  (:after evil-nerd-commenter
+   :nv "#" #'evilnc-comment-or-uncomment-lines)
+  (:after vertico
+          (:leader
+           :desc "Repeat last Vertico search" "\"" #'vertico-repeat-select)
+          ))
+ (:after centaur-tabs
+  :nv "S-<left>" #'centaur-tabs-backward
+  :nv "S-<right>" #'centaur-tabs-forward
+  :nv "C-SPC" #'centaur-tabs-switch-group
+  :nv "C-t" #'centaur-tabs--create-new-tab
+  :nv "s-{" #'centaur-tabs-backward
+  :nv "s-}" #'centaur-tabs-forward
+  (:leader
+   (:prefix ("TAB" . "Tab")
+    :desc "Switch Tab" "TAB" #'centaur-tabs-switch-group
+    :desc "Close Tab" "x" #'centaur-tabs-kill-all-buffers-in-current-group
+    :desc "New Tab" "n" #'centaur-tabs--create-new-tab
+    )))
+ (:after dap-mode
+  :localleader
+  (:prefix ("d" . "debug")
+   :desc "DAP Debug" "d" #'dap-debug
+   :desc "DAP Hydra" "h" #'dap-hydra
+   :desc "DAP Disconnect" "Q" #'dap-disconnect
+   (:prefix ("b" . "breakpoint")
+    :desc "DAP Breakpoint Add" "a" #'dap-breakpoint-add
+    :desc "DAP Breakpoint Delete" "d" #'dap-breakpoint-delete
+    :desc "DAP Breakpoint Delete All" "D" #'dap-breakpoint-delete
+    )))
+ )
 
-(map! :mode flycheck
-      :mode flycheck-mode-map
-      :localleader
+(map! :after flycheck
+      (:leader
       (:prefix ("e" . "errors")
-       :desc "show buffer errors" "l" #'flycheck-list-errors
        :desc "go to next error" "n" #'flycheck-next-error
        :desc "go to previous error" "p" #'flycheck-previous-error
-       ))
-
-(map! :mode flymake
-      :map flymake-mode-map
-      :localleader
-      (:prefix ("e" . "errors")
-       :desc "show buffer errors" "l" #'flymake-show-buffer-diagnostics
-       :desc "show project errors" "L" #'flymake-show-project-diagnostics
-       :desc "go to next error" "n" #'flymake-goto-next-error
-       :desc "go to previous error" "p" #'flymake-goto-prev-error
-       ))
-
-;; (map! :mode flymake
-;;       :map flymake-mode-map
-;;       :localleader
-;;       (:prefix ("e" . "errors")
-;;        :desc "show buffer errors" "l" #'flymake-show-buffer-diagnostics
-;;        :desc "show project errors" "L" #'flymake-show-project-diagnostics
-;;        :desc "go to next error" "n" #'flymake-goto-next-error
-;;        :desc "go to previous error" "p" #'flymake-goto-prev-error
-;;        ))
-(map! :after flycheck
-      :n "SPC e n" #'flycheck-next-error
-      :n "SPC e p" #'flycheck-previous-error
-      :n "SPC e v" #'flycheck-verify-setup
-      :n "SPC e l" #'flycheck-list-errors)
+       :desc "verify Flycheck setup" "v" #'flycheck-verify-setup
+       :desc "list errors" "l" #'flycheck-list-errors)))
 
 (map! :after vmd
       :mode markdown-mode
-      :n "SPC m p" #'vmd-mode)
+      (:localleader
+       :desc "VMD mode" "p" #'vmd-mode))
 
 (map! :after rustic
       :map rustic-mode-map
-      :localleader
-      (:prefix ("c" . "check")
+      (:localleader
+       (:prefix ("c" . "check")
         :desc "cargo check" "c" (cmd! (compile "cargo check"))
-        ))
+        )))
 
 (map! :after org
       :map org-mode-map
-      :nv "SPC m -" #'org-cycle-list-bullet
       :i "TAB" #'evil-org->
       :i "S-TAB" #'evil-org-<
       :nv "C-j" #'org-move-subtree-down
       :nv "C-k" #'org-move-subtree-up
-      :i "RET" #'evil-org-return)
+      :i "RET" #'evil-org-return
+      (:localleader
+       :nv "-" #'org-cycle-list-bullet))
 
 (map! :after evil-embrace
       :v "v" #'er/expand-region
       :v "V" #'er/contract-region)
 
-(map! :after ponylang-mode
-      :map ponylang-mode-map
-      :nv "SPC m" #'ponylang-menu)
-
 (map! :after smerge-mode
       :map smerge-mode-map
-      :nv "SPC g n" #'smerge-next
-      :nv "SPC g p" #'smerge-prev
-      :nv "SPC g M" #'smerge-keep-mine
-      :nv "SPC g m" #'smerge-keep-mine
-      :nv "SPC g T" #'smerge-keep-lower
-      )
+      (:leader
+       (:prefix ("g" . "smerge")
+        :desc "next hunk" "n" #'smerge-next
+        :desc "prev hunk" "p" #'smerge-prev
+        :desc "keep my version" "M" #'smerge-keep-mine
+        :desc "keep my version" "m" #'smerge-keep-mine
+        :desc "keep their version" "T" #'smerge-keep-lower
+        )))
 
 (map! :mode rfc-mode
       :map rfc-mode-map
